a2aa3bb9807ea329c1689938ec43540efa7740da
Rory Flint <rory@roryflint.co.uk>
2019-09-23 15:12:05 +0100

    create Knex migrations for tabs table

--- database/seeds/002createTabsTable.js
+++ database/seeds/002createTabsTable.js
@@ -1,5 +1,6 @@
 
 exports.seed = function (knex) {
+  // Deletes ALL existing entries
   return knex('users').del()
     .then(() => {
       return knex('users').insert([
---
+++ tabFiles/tabRoutes.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+// const db = require('./tabHelpers');
+// const middleware = require('./tabMiddleware');
+
+// const Router = express.Router();
+
+module.exports = {
+
+};
