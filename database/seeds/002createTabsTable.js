a91bd46c0ea3fa648e3a04e1913e8cc8883358ba
Rory Flint <rory@roryflint.co.uk>
2019-09-23 15:36:17 +0100

    simple seeding for tabs complete

--- database/seeds/002createTabsTable.js
+++ database/seeds/002createTabsTable.js
@@ -1,17 +1,28 @@
 
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
-  return knex('users').del()
+  return knex('tabs').del()
     .then(() => {
-      return knex('users').insert([
+      return knex('tabs').insert([
         {
-          id: 1, email: 'rory@rory.com', password: '123456', name: 'Rory',
+          id: 1, url: 'https://www.google.com', title: 'Google Homepage', notes: 'The evil homepage that everybody cannot help but use', user_id: 1,
         },
         {
-          id: 2, email: 'anna@anna.com', password: '123456', name: 'Anna',
+          id: 2, url: 'https://www.reddit.com', title: 'Reddit', notes: 'Reddit - Frontpage of the internet', user_id: 1,
         },
         {
-          id: 3, email: 'gabe@postman.com', password: '123456', name: 'Gabe',
+          id: 7, url: 'https://www.theguardian.com/books/2019/sep/20/stieg-larsson-and-the-unsolved-case-of-olof-palme', title: 'Stieg Larsson article', notes: 'About the Prime Minister who was assassinated.', user_id: 1,
+        },
+        {
+          id: 3, url: 'https://www.youtube.com/watch?v=hiiEeMN7vbQ&t=274s', title: 'Developing a Growth Mindset with Carol Dweck', notes: 'Video recommended by Isaac', user_id: 1,
+        },
+        {
+          id: 4, url: 'https://open.spotify.com/search/time', title: 'Hans Zimmer Song', notes: 'From that space film', user_id: 2,
+        },
+        {
+          id: 5, url: 'https://codeselfstudy.com/blog/deploy-node-postgres-heroku/', title: 'Postgres blog', notes: 'How to deploy a postgres app to Heroku', user_id: 2,
+        },
+        {
+          id: 6, url: 'https://learn.lambdaschool.com/web4node/module/rec5J23yAIdjmFbgP/', title: 'Lambda TK on Data', notes: 'Used this a lot in building this backend.', user_id: 2,
         },
       ]);
     });
