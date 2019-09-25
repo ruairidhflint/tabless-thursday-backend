60d10707472b1b89bf0fd413d6621a867cfa205a
Rory Flint <rory@roryflint.co.uk>
2019-09-25 10:01:40 +0100

    error handling file updated

---
+++ errorHandling/errors.js
@@ -0,0 +1,27 @@
+const invalidPermissions = 'Invalid Permissions';
+const allFieldsPresent = 'Please sure all required fields are present.';
+const successfulDeletion = 'Successful Deletion';
+const invalidLoginDetails = 'Invalid Login Credentials';
+const error = 'Error of some kind. Refer to API documentation.';
+const inValidTabID = 'Invalid ID for Tab';
+const noSavedTabs = 'No saved Tabs yet';
+const emailUsed = 'This email has already been used!';
+const invalidEmaiLFormat = 'Please enter a valid email address.';
+const passwordFormat = 'Password must be at least 8 characters long';
+const postSuccess = 'Post success';
+const postFailed = 'Post failed.';
+
+module.exports = {
+  invalidPermissions,
+  allFieldsPresent,
+  successfulDeletion,
+  invalidLoginDetails,
+  error,
+  inValidTabID,
+  noSavedTabs,
+  emailUsed,
+  invalidEmaiLFormat,
+  passwordFormat,
+  postSuccess,
+  postFailed,
+};
