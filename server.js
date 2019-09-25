ee593819b2de9898172ebd4146288815e0cdc5fe
Rory Flint <rory@roryflint.co.uk>
2019-09-25 08:49:46 +0100

    Installed Helmet as dependency and used

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
 const cors = require('cors');
 const userRoutes = require('./userFiles/userRoutes');
 const tabRoutes = require('./tabFiles/tabRoutes');
 
 const server = express();
 
+server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use('/users', userRoutes);
